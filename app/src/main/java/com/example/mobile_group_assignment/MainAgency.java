2e3da5b345b81dd048d434a45149866a5208d013
James <jamestan21403@gmail.com>
2025-04-22 15:50:28 +0800

    File Initialize

---
+++ app/src/main/java/com/example/mobile_group_assignment/MainAgency.java
@@ -0,0 +1,14 @@
+package com.example.mobile_group_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainAgency extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_agency);
+    }
+}
