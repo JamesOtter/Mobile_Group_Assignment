3d9182074134b7ab343973541cccb10b332ac614
James <jamestan21403@gmail.com>
2025-04-22 22:29:42 +0800

    Initialize firebase

--- app/src/main/java/com/example/mobile_group_assignment/MainActivity.java
+++ app/src/main/java/com/example/mobile_group_assignment/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.mobile_group_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase
+        FirebaseApp.initializeApp(this);
+
+//        Button btnTravelPlaces = findViewById(R.id.btnTravelPlaces);
+//        btnTravelPlaces.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Navigate to TravelPlacesActivity
+//                Intent intent = new Intent(MainActivity.this, MainAgency.class);
+//                startActivity(intent);
+//
+//                // Optional: Add animation
+//                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+//            }
+//        });
     }
 }
